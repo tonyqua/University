4447eed1e3eaf5527e71243ffa5a3a19e8f77fb5
TonyQ <150294902+tonyqua@users.noreply.github.com>
2024-04-04 21:17:52 +0300

    Add files via upload

---
+++ dviykoviPerevod.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Anton on 04.04.2024.
+//
+#include <iostream>
+#include <cmath>
+
+int main() {
+    int num;
+    std::cout << "Введите десятичное число: ";
+    std::cin >> num;
+
+    int size = log2(num) + 1;
+    int* arr = new int[size];
+
+    for(int i = size - 1; i >= 0; i--) {
+        arr[i] = num % 2;
+        num /= 2;
+    }
+
+    std::cout << "Двоичное число: ";
+    for(int i = 0; i < size; i++) {
+        std::cout << arr[i];
+    }
+
+    delete[] arr;
+    return 0;
+}
